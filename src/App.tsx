7c49df88dfa4b0f9b311f4fa0e25ae5d5150ad79
Olha Romanova <larklarky@gmail.com>
2022-10-31 14:03:48 +0100

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './App.module.css';
+import {IoClose, IoMenu} from 'react-icons/io5'
 
 function App() {
+  const [active, setActive] = useState<boolean>(false);
   return (
     <div className="App">
+      <img
+          src='/image-hero-desktop.jpg'
+          srcSet="/image-hero-mobile.jpg 700w, /image-hero-desktop.jpg 1440w"
+          sizes="(max-width: 700px) 700px, 1440px"
+          alt="desktop"
+        />
+      
+      <div className={styles.navbar}>
+        <div className={styles.logo}>crowdfund</div>
+        <div className={styles.links}>
+        <button 
+          className={`${styles.menu}`}
+          onClick={() => setActive(!active)}
+        >
+          {active ? <IoClose/> : <IoMenu/>}
+        </button>
+          <ul className={active ? styles.dropped : ''}>
+            <li>About</li>
+            <li>Discover</li>
+            <li>Get Started</li>
+          </ul>
+        </div>
+      </div>
       
     </div>
   );
